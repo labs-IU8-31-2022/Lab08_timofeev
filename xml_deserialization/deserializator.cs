df346de06d1d5d651d4d76173aaf4f4a4f34b6e8
undefinedev <undefined_dev@inbox.ru>
2022-10-31 03:27:46 +0300

    first task complete

---
+++ xml_deserialization/deserializator.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Serialization;
+using animal;
+
+var xmlSerializer = new XmlSerializer(typeof(Cow));
+using (var file = new FileStream("Cow.xml", FileMode.OpenOrCreate))
+{
+    var cow = xmlSerializer.Deserialize(file) as Cow;
+    Console.WriteLine("deserialized");
+    Console.WriteLine($"{cow?.Country} {cow?.HideFromOtherAnimals} {cow?.Name} {cow?.WhatAnimal}");
+}
---
+++ xml_serialization/serializator.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+using animal;
+
+Animal ex = new Cow("Russia", true, "Burenka", eClassificationAnimal.Herbivores);
+
+var xmlSerializer = new XmlSerializer(typeof(Cow));
+using (var file = new FileStream("Cow.xml", FileMode.OpenOrCreate))
+{
+    xmlSerializer.Serialize(file, ex);
+    Console.WriteLine("serialized");
+}
